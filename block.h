15556867c3dfaed41230968b36712db986abf87b
221evans <jayevo95@gmail.com>
2024-05-16 21:23:27 +0100

    Add Position, Block, and Color classes
    This commit introduces three new classes, namely Position, Block, and Color. The Position class has variables row and column, the Block class holds several properties including id and cellSize, and has a std::map of Positions. The Color class also defines several Color constants. These classes are also integrated into the CMakeLists.txt file.

---
+++ block.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <map>
+#include "position.h"
+
+
+class Block
+{
+public:
+    Block();
+    int id;
+    std::map<int, std::vector<Position>> cells;
+
+private:
+    int cellSize;
+    int rotationState;
+
+};
+
