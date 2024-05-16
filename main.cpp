5cd3d73fdc25561e2cadd7fcc365641353ae5dc3
221evans <jayevo95@gmail.com>
2024-05-16 20:53:27 +0100

    Add Grid class and update main file
    A new Grid class has been added including its header file 'grid.h' and implementation file 'grid.cpp'. This class has been incorporated into the 'main.cpp' file. The build files have been updated accordingly to reflect these changes.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-#include <iostream>
-#include "raylib.h"
+#include <raylib.h>
+#include "grid.h"
 int main() {
 
-
-    InitWindow(300,600, "Raylib tetris");
+    Color darkBlue = {44,44,127,255};
+    InitWindow(300,600, "Raylib Tetris");
     int setTargetFPS(60);
+
+    Grid grid = Grid();
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
+    grid.Print();
 
     while(!WindowShouldClose())
     {
         BeginDrawing();
-        
+        ClearBackground(darkBlue);
+        grid.Draw();
         EndDrawing();
     }
 
